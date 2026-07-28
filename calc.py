e591a421a34addc3fa89c38da6ee11f8cac0256a
ritika1221 <ritikxa3012@gmail.com>
2026-07-28 20:57:51 +0530

    Implement basic calculator operations in calc.py

---
+++ calc.py
@@ -0,0 +1,23 @@
+print("1 - Add")
+print("2 - Subtract")
+print("3 - Multiply")
+print("4 - Divide")
+option=input("choose an operation:")
+result=0
+
+if(option in [1,2,3,4]):
+    num1 = int(input("enter first number:"))
+    num2=int(input("enter second number: "))
+
+    if(option == "1"):
+        result = num1 + num2
+    elif(option == "2"):
+        result = num1 - num2
+    elif(option == "3"):
+        result = num1 * num2
+    elif(option =="4"):
+        result = num1 // num2
+
+
+else:
+    print("invalid operation entered")
